869fb8aa785c932e7cf9d9cfb212db14d269c095
Alex Sheiko <82119411+by-ultra-dev@users.noreply.github.com>
2023-03-31 12:27:50 +0300

    fix: function handleSelect

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,6 +1,6 @@
-import Select, { OnChangeValue } from 'react-select';
-import { customStyles } from './styles';
+import Select from 'react-select';
 import { SelectOption } from '../../types/types';
+import { customStyles } from './styles';
 
 interface SelectProps {
   value: string;
@@ -9,13 +9,13 @@ interface SelectProps {
 
 export const CustomSelect = ({ value, onChange }: SelectProps) => {
   const options: SelectOption[] = [
-    { value: '1.1', label: '10%' },
-    { value: '1.15', label: '15%' },
-    { value: '1.2', label: '20%' },
+    { value: '10', label: '10%' },
+    { value: '15', label: '15%' },
+    { value: '20', label: '20%' },
   ];
 
-  const handleSelect = (newValue: OnChangeValue<SelectOption, boolean>) => {
-    onChange((newValue as SelectOption).value);
+  const handleSelect = (newValue: SelectOption | null) => {
+    newValue?.value && onChange(newValue.value);
   };
 
   const getOptionValue = (value: string): SelectOption | undefined => {
@@ -28,6 +28,7 @@ export const CustomSelect = ({ value, onChange }: SelectProps) => {
       options={options}
       value={getOptionValue(value)}
       onChange={handleSelect}
+      isMulti={false}
     />
   );
 };
